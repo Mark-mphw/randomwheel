003cc56cc7f012252ca589eda20227fcb451ad34
Mark.mphw <70524457+Mark-mphw@users.noreply.github.com>
2024-01-25 14:09:38 +0700

    random wheel

---
+++ android/app/src/main/kotlin/com/example/ramdom_rewards/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ramdom_rewards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/common/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static List<String> get fortuneValues => const <String>[
+        '1st',
+        '2nd',
+        '3rd',
+        '4th',
+      ];
+}
